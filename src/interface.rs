f8528cf19f2ace31aef9191814c2267d839a70d7
Colin Stanfill <colin.stanfill@gmail.com>
2016-12-12 18:45:49 -0500

    Input names of people at the table
    Tab goes between input states, enter starts entering a new value for a
    field and also finished entering a field.

--- src/interface.rs
+++ src/interface.rs
@@ -5,6 +5,7 @@ use data::{Table, Entry};
 use parse::{parse_input, get_error_cursor};
 use score::Score;
 use self::pancurses::{Input, Window};
+use std::mem;
 
 const CURSOR_NORMAL     : i16 = 1;
 const CURSOR_ERROR      : i16 = 3;
@@ -15,6 +16,7 @@ const CURSOR_CONTRACT   : i16 = 5;
 const CURSOR_UNDERTRICK : i16 = 2;
 const CURSOR_ENTERING   : i16 = 6;
 
+#[derive(Copy, Clone)]
 enum EntryField {
     Number,
     Name,
@@ -23,10 +25,18 @@ enum EntryField {
     Result,
 }
 
+#[derive(Copy, Clone)]
 enum Selection {
+    Unselected,
     NameSelect(Seat),
     // On an entry
     FieldSelect(i32, EntryField),
+}
+
+enum FieldStatus {
+    NotSelected,
+    Selected,
+    Entering,
 }
 
 struct InputState {
@@ -37,19 +47,47 @@ struct InputState {
 impl InputState {
     pub fn new() -> InputState {
         InputState {
-            selection_ : Selection::NameSelect(Seat::North),
+            selection_ : Selection::Unselected,
             entry_ : None,
         }
     }
+
+    pub fn is_seat(&self, seat : Seat) -> FieldStatus {
+        match (self.selection_, self.entry_.as_ref()) {
+            (Selection::NameSelect(s), Some(_)) if s == seat =>
+                FieldStatus::Entering,
+            (Selection::NameSelect(s), None) if s == seat =>
+                FieldStatus::Selected,
+            _ => FieldStatus::NotSelected,
+        }
+    }
+
+    pub fn is_north(&self) -> FieldStatus {
+        self.is_seat(Seat::North)
+    }
+
+    pub fn is_east(&self) -> FieldStatus {
+        self.is_seat(Seat::East)
+    }
+
+    pub fn is_south(&self) -> FieldStatus {
+        self.is_seat(Seat::South)
+    }
+
+    pub fn is_west(&self) -> FieldStatus {
+        self.is_seat(Seat::West)
+    }
 }
+
 pub struct Interface {
     // Contract-related
     table_: Table,
     dealer_ : Seat,
     entries_ : Vec<Entry>,
-    input_state_ : Option<InputState>,
+    input_state_ : InputState,
 
     root_window_ : Window,
+    entry_window_ : Window,
 
     table_window_ : Window,
     table_border_window_ : Window,
@@ -97,17 +135,29 @@ fn draw_input(input : &str, window : Window, y : i32, x : i32) {
     }
 }
 
+fn set_field_cursor(window : &Window, is_selected : FieldStatus) {
+    window.attroff(pancurses::A_UNDERLINE);
+    window.color_set(CURSOR_NORMAL);
+    match is_selected {
+        FieldStatus::NotSelected => 0,
+        FieldStatus::Selected => window.attron(pancurses::A_UNDERLINE),
+        FieldStatus::Entering => window.color_set(CURSOR_ENTERING),
+    };
+}
+
 impl Interface {
     pub fn new(root_window : Window) -> Interface {
         let tablewin = root_window.derwin(9, 15, 1, 0).unwrap();
         let valueswin = root_window.derwin(10, 15, 0, 14).unwrap();
+        let entrywin = root_window.derwin(1, 14, 0, 0).unwrap();
         let entrieswin = root_window.derwin(40, 40, 10, 0).unwrap();
         Interface {
             table_ : Table::new(),
             dealer_ : Seat::North,
             entries_ : Vec::new(),
-            input_state_ : None,
+            input_state_ : InputState::new(),
             root_window_ : root_window,
+            entry_window_ : entrywin,
             table_window_ : shrink(&tablewin),
             table_border_window_ : tablewin,
             values_window_ : shrink(&valueswin),
@@ -120,6 +170,12 @@ impl Interface {
     pub fn redraw(&self) {
         self.draw_borders();
         self.draw_table();
+        if self.input_state_.entry_.is_some() {
+            pancurses::curs_set(1);
+        } else {
+            pancurses::curs_set(0);
+        }
+        self.draw_input();
         self.refresh();
     }
 
@@ -132,18 +188,55 @@ impl Interface {
         match c {
             '\t' => self.cycle_input(),
             '\n' => self.enter_input(),
-            _    => (),
+            _    => self.input_char(c),
         };
         return true;
     }
 
     fn cycle_input(&mut self) {
-        if self.input_state_.is_none() {
-            self.input_state_ = Some(InputState::new());
-        }
+        let new_selection = match self.input_state_.selection_ {
+            Selection::Unselected => Selection::NameSelect(Seat::North),
+            Selection::NameSelect(Seat::North) =>
+                Selection::NameSelect(Seat::East),
+            Selection::NameSelect(Seat::East) =>
+                Selection::NameSelect(Seat::South),
+            Selection::NameSelect(Seat::South) =>
+                Selection::NameSelect(Seat::West),
+            Selection::NameSelect(Seat::West) => Selection::Unselected,
+            _ => Selection::Unselected,
+        };
+
+        self.input_state_.selection_ = new_selection;
+        self.input_state_.entry_ = None;
     }
 
     fn enter_input(&mut self) {
+        match self.input_state_.selection_ {
+            Selection::Unselected => { return; },
+            _ => (),
+        };
+        match self.input_state_.entry_ {
+            None => self.input_state_.entry_ = Some("".to_string()),
+            Some(_) => {
+                let input = mem::replace(&mut self.input_state_.entry_, None);
+                self.process_input(input.unwrap());
+            },
+        };
+    }
+
+    fn process_input(&mut self, input : String) {
+        match self.input_state_.selection_ {
+            Selection::Unselected => panic!("How did you do that"),
+            Selection::NameSelect(s) => self.table_.set_player(s, &input),
+            Selection::FieldSelect(_, _) => (),
+        }
+    }
+
+    fn input_char(&mut self, c : char) {
+        match c {
+            '\x7f' => {self.input_state_.entry_.as_mut().unwrap().pop();},
+            _      => self.input_state_.entry_.as_mut().unwrap().push(c),
+        }
     }
 
     fn north(&self) -> &str {
@@ -162,6 +255,11 @@ impl Interface {
         self.table_.get_player(Seat::West)
     }
 
+    fn input(&self) -> &str {
+        // Awkward.
+        self.input_state_.entry_.as_ref().map(String::as_str).unwrap_or("")
+    }
+
     fn draw_borders(&self) {
         border(&self.values_border_window_);
         border(&self.table_border_window_);
@@ -171,9 +269,14 @@ impl Interface {
     fn draw_table(&self) {
         let table_win = &self.table_window_;
         let midpoint = table_win.get_max_y() / 2;
+        &table_win.clear();
+        set_field_cursor(&table_win, self.input_state_.is_north());
         center_pad(&table_win, self.north(), 0);
+        set_field_cursor(&table_win, self.input_state_.is_south());
         center_pad(&table_win, self.south(), table_win.get_max_y() - 1);
+        set_field_cursor(&table_win, self.input_state_.is_east());
         right_justify(&table_win, self.east(), midpoint);
+        set_field_cursor(&table_win, self.input_state_.is_west());
         left_justify(&table_win, self.west(), midpoint);
     }
 
@@ -230,6 +333,17 @@ impl Interface {
         entry_win.addch('\n');
     }
 
+    fn draw_input(&self) {
+        self.entry_window_.mv(0, 0);
+        self.entry_window_.clear();
+        let mut x = 0;
+        for c in self.input().chars() {
+            self.entry_window_.addch(c);
+            x = x + 1;
+        }
+        self.root_window_.mv(0, x);
+    }
+
     fn refresh(&self) {
         self.values_border_window_.refresh();
         self.entries_border_window_.refresh();
@@ -239,6 +353,7 @@ impl Interface {
         self.table_window_.refresh();
         self.values_window_.refresh();
         self.values_window_.refresh();
+        self.entry_window_.refresh();
         self.root_window_.refresh();
     }
 }
